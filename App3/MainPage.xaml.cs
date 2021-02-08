19fae34da4f68a0968f1c8c50a5c0074103228c5
Subindev Vasudevan <subindev.vasudevan@gac.com>
2021-02-08 23:06:10 +0400

    Add project files.

---
+++ App3/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            webview.Source = new Uri(urlBox.Text);
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            webview1.Source = new Uri(urlBox1.Text);
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            webview2.Source = new Uri(urlBox2.Text);
+        }
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            webview3.Source = new Uri(urlBox3.Text);
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Web.WebView2.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> websites = new List<string>()
+        {
+            "https://www.microsoft.com",
+            "https://apple.com",
+            "https://youtube.com",
+            "https://office.com",
+            "https://netflix.com"
+        };
+
+        Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            var webview = new WebView2()
+            {
+                Source = new Uri(websites[rnd.Next(0, websites.Count -1)]),
+            };
+            var newgrid = new Grid()
+            {
+                Margin = new Thickness(5),
+                Width = 500
+            };
+            newgrid.Children.Add(webview);
+            stackPanel.Children.Add(newgrid);
+        }
+    }
+}
